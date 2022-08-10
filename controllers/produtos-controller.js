75e78e3cdbb1c30d7ecb6942d63892ad486c8cca
Ariel Campos <ariel.campos94@yahoo.com>
2022-08-09 23:26:33 -0300

    criado fetch consumindo produtos database.json

---
+++ controllers/produtos-controller.js
@@ -0,0 +1,31 @@
+import { produtoServices } from "../services/produto-servicos.js";
+
+const novoProduto = (name, price, imageUrl, alt) => {
+    const card = document.createElement("div");
+    card.className = "produto";
+
+    const conteudo = `
+        <img class="produto__imagem" src="${imageUrl}" alt="${alt}">
+        <p class="produto__descricao">${name}</p>
+        <p class="produto__preco">R$ ${Number(price).toFixed(2)}</p>
+        <a class="produto__link" href="#">Ver produto</a>
+    `; 
+    card.innerHTML = conteudo;
+    return card;
+}
+
+const produtos = document.querySelector("[data-product]");
+const render = async () => {
+    try {
+        const listaProdutos = await produtoServices.listaProdutos();
+        console.log(listaProdutos);
+        listaProdutos.forEach(elemento => {
+            produtos.appendChild(novoProduto(elemento.name, elemento.price, elemento.imageUrl, elemento.alt));
+        });
+        
+    } catch (erro) {
+       console.error(erro); 
+    }
+}
+
+render();
---
+++ services/produto-servicos.js
@@ -0,0 +1,8 @@
+// Leitura da database
+const listaProdutos = () =>
+    fetch("http://localhost:3000/produto")
+    .then(resposta => resposta.json());
+
+export const produtoServices = {
+    listaProdutos
+}
